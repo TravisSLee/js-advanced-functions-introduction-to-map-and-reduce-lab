b72c3902a6c272244b84c00f56171893aa3c6cd6
TravisSLee <travisslee223@gmail.com>
2021-03-02 19:28:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function mapToNegativize(arr){
+    let negative = arr.map( num => -num )
+    return negative
+}
+
+function mapToNoChange(arr){
+    let non = arr.map( num => num )
+    return non
+}
+
+function mapToDouble(arr){
+    let dub = arr.map( num => num * 2)
+    return dub
+}
+
+function mapToSquare(arr){
+    let sq = arr.map( num => num ** 2)
+    return sq
+}
+
+function reduceToTotal(arr, start = 0){
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let tot = arr.reduce(reducer, start)
+    return tot
+}
+
+function reduceToAllTrue(arr){
+    const testing = (currentValue) => currentValue == true;
+    return arr.every(testing)
+}
